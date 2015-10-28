1e9200a9b13ac207403e8bf9f8295d8497612acb
Ed Luijendijk <e.luijendijk@caesar.nl>
2015-10-28 08:52:13 +0000

    Same

--- PUG screen Hybrid/components/dataListViewOrd/index.js
+++ PUG screen Hybrid/components/dataListViewOrd/index.js
@@ -35,10 +35,54 @@ app.dataListViewOrd = kendo.observable({
         dataSource = new kendo.data.DataSource({
             pageSize: 50
         }),
+		
         dataListViewOrdModel = kendo.observable({
             dataSource: dataSource,
             dataSourceOptions: dataSourceOptions,
-            jsdoOptions: jsdoOptions
+            jsdoOptions: jsdoOptions,
+            itemClick: function(e) {
+                app.mobileApp.navigate('#components/dataListViewOrd/details.html?uid=' + e.dataItem.uid);
+			},
+			
+			detailsShow: function(e) {
+                var item = e.view.params.uid,
+                    dataSource = dataListViewOrdModel.get('dataSource'),
+                    itemModel = dataSource.getByUid(item);
+                if (!itemModel.OrdNum) {
+                    itemModel.OrdNum = String.fromCharCode(160);
+                }
+                dataListViewOrdModel.set('currentItem', itemModel);
+            },
+                        /* MAS */
+        submit :  function(e) {
+            console.log("submit");
+            var jsdo = dataListViewOrdModel.dataSource.transport.jsdo;
+            var jsrow = jsdo.findById(dataListViewOrdModel.currentItem._id);
+             var afterUpdateFn;
+                jsrow.assign(dataListViewOrdModel.currentItem);
+           
+            afterUpdateFn = function(jsdo, record, success, request) {
+                /* unsubscribe so this fn doesn't execute for some other event */              
+                jsdo.unsubscribe('afterUpdate', afterUpdateFn);
+               
+                if (success === true) {
+                app.mobileApp.navigate('#:back');
+                  
+                }
+                else {
+                    cError = "Update Error: " + dataListViewOrdModel.normalizeError(request, record);
+                    app.showError(cError);
+                    console.log(cError);
+                }
+           };
+                       jsdo.subscribe('afterUpdate', afterUpdateFn);
+ 
+            jsdo.saveChanges();          
+        },
+        cancel : function(e) {console.log("cancel")},
+        /* end MAS */
+ 
+            currentItem: null
         });
 
     parent.set('dataListViewOrdModel', dataListViewOrdModel);
